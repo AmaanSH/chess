09a7efa159c8d4f2291cf04efd312ad8b824e6bd
Amaan <amaanshawkath@legendary-games.com>
2018-06-08 17:12:36 +0100

    addition of winning file

---
+++ assets/js/winning.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// TO DO
+// how many pieces left on board?
+// How many pieces have each team collected
+// Is the King still on the board? (is it in check?)
+
+console.log('winning module connected');
+
+function checkWinningParameters() {
+
+}
+
+function checkMateCheck() {
+
+}
